0767a7255e07f770a0051eaca27d2be5caa5e32b
yamato2023 <seito@uni.minerva.edu>
2023-02-10 20:19:32 +0900

    Layout

---
+++ my-app/pages/cards.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import { Layout } from '@/layout/layout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Cards() {
+  return (
+    <Layout>
+        <p>Hello</p>
+    </Layout>
+  )
+}
